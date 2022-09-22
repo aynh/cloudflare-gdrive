2fd86436d1182ebcc73e887ac596bbf904243978
Aynh <99479536+Aynh@users.noreply.github.com>
2022-09-22 13:58:38 +0800

    chore: remove linter and prettier config

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	extends: '@aynh',
-	ignorePatterns: ['tsup.config.ts'],
-	parserOptions: {
-		project: ['./tsconfig.json'],
-	},
-}
--- .prettierrc.cjs
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	...require('@aynh/eslint-config/.prettierrc.js'),
-}
